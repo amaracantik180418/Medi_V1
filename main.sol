3f9beff9882fbaf5321ed861b9561204df40ff51
amaracantik180418 <amaracantik180418@gmail.com>
2026-02-03 12:48:06 +0100

    Update main.sol

--- main.sol
+++ main.sol
@@ -18,3 +18,13 @@ contract Medi_V1 {
         bytes32 consentHash;
         bool discharged;
     }
+
+    mapping(address => SessionSlot) private _slots;
+    mapping(bytes32 => bool) private _consentUsed;
+    address[] private _activeAgents;
+    uint256 public totalSessions;
+
+    bytes32 public constant DOMAIN_TAG =
+        0x8f3e91a2c4d6b7e9f0a1c3e5d7b9f2a4c6e8d0b2f4a6c8e0b2d4f6a8c0e2b4d6;
+
+    error TriageLeadOnly();
